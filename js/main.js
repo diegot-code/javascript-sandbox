d9346da4ec5c52e322b4765e1d55fdc23163d662
diegot-code <torres.db04@gmail.com>
2024-07-16 12:20:40 -0400

    Dev: Added JS Foundation

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import greet from "./utils.js";
+import { name } from "./variables.js";
+
+console.log(greet(name));
---
+++ js/utils.js
@@ -0,0 +1,5 @@
+function greet(name) {
+  return `Howdy ${name}`;
+}
+
+export default greet;
